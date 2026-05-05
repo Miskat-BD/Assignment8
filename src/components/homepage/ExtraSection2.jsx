a33abc2a115fc66eca4815039879ccef3d2a39eb
Miskat-BD <mishkatahmed321@gmail.comgit config --global user.name Miskat-BDgit commit -m first>
2026-05-05 19:53:12 +0600

    Extra section 2 - What Our Readers Say add hover

--- src/components/homepage/ExtraSection2.jsx
+++ src/components/homepage/ExtraSection2.jsx
@@ -11,7 +11,7 @@ const ExtraSection2 = () => {
             <h1 className='text-4xl text-center font-bold'>What Our Readers Say</h1>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6 px-4 py-10">
 
-                <div className="p-6 bg-white shadow rounded-lg text-center border border-purple-200">
+                <div className="p-6 bg-white shadow rounded-lg text-center border border-purple-200 hover:shadow-xl transition">
                     <p className="text-gray-500 mb-4">
                         BookNest made it super easy to find and borrow books.
                         The experience is smooth and fast.
@@ -21,7 +21,7 @@ const ExtraSection2 = () => {
                     <p className="text-yellow-500 text-center flex justify-center gap-1"><FaStar /> <FaStar /><FaStar /><FaStar /><FaStar /></p>
                 </div>
 
-                <div className="p-6 bg-white shadow rounded-lg text-center border border-purple-200">
+                <div className="p-6 bg-white shadow rounded-lg text-center border border-purple-200 hover:shadow-xl transition">
                     <p className="text-gray-500 mb-4">
                         I love the clean design and easy navigation.
                         It feels like a real digital library.
@@ -31,7 +31,7 @@ const ExtraSection2 = () => {
                     <p className="text-yellow-500 text-center flex justify-center gap-1"><FaStar /> <FaStar /><FaStar /><FaStar /><FaStar /></p>
                 </div>
 
-                <div className="p-6 bg-white shadow rounded-lg text-center border border-purple-200">
+                <div className="p-6 bg-white shadow rounded-lg text-center border border-purple-200 hover:shadow-xl transition">
                     <p className="text-gray-500 mb-4">
                         Finding books by category is খুব easy.
                         Highly recommended for book lovers!
