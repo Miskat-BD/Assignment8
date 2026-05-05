2fa1df8741b061c57c2c66801e6a09ea93e82137
Miskat-BD <mishkatahmed321@gmail.comgit config --global user.name Miskat-BDgit commit -m first>
2026-05-05 22:24:11 +0600

    book details section running

--- src/app/books/[id]/page.jsx
+++ src/app/books/[id]/page.jsx
@@ -1,9 +1,15 @@
+import { getAllBooks } from '@/lib/data';
 import React from 'react';
 
-const BookDetailsPage = () => {
+const BookDetailsPage = async ({ params }) => {
+    const { id } = await params;
+    const books = await getAllBooks();
+    const book = books.find(book => book.id == id)
+    console.log(books, 'ref');
     return (
         <div>
             <h1>Book Details Page</h1>
+            <h1>{book.title}</h1>
         </div>
     );
 };
