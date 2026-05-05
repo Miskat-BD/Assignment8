7ac821e5dd1b7d5cdc8e1ac2fa94434b60902f8e
Miskat-BD <mishkatahmed321@gmail.comgit config --global user.name Miskat-BDgit commit -m first>
2026-05-05 15:23:27 +0600

    navbar left and center done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,36 @@
+import Link from 'next/link';
 import React from 'react';
+import NavLink from './NavLink';
 
 const Navbar = () => {
+    const links = <>
+        <li><NavLink href={'/'}>Home</NavLink></li>
+        <li><NavLink href={'/books'}>All Books</NavLink></li>
+        <li><NavLink href={'/my-profile'}>My Profile</NavLink></li>
+    </>
     return (
-        <div>
-            <h1>Navbar</h1>
+        <div className="navbar bg-base-100 shadow-sm">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex="-1"
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        {links}
+                    </ul>
+                </div>
+                <Link href={'/'} className='text-3xl font-bold bg-linear-to-l from-[#9514FA] to-[#4F39F6] bg-clip-text text-transparent'>BookNest</Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 gap-2">
+                    {links}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <Link href={'login'} className="btn bg-linear-to-l from-[#9514FA] to-[#4F39F6] text-white">Login</Link>
+            </div>
         </div>
     );
 };
