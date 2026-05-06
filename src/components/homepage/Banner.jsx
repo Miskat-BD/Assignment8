ac1bd36c519e650941ff67a93adc473b07c7cafd
Miskat-BD <mishkatahmed321@gmail.comgit config --global user.name Miskat-BDgit commit -m first>
2026-05-06 20:16:36 +0600

    animation add on banner section

--- src/components/homepage/Banner.jsx
+++ src/components/homepage/Banner.jsx
@@ -13,7 +13,9 @@ const Banner = () => {
                         width={600} height={550}
                     />
                     <div>
-                        <h1 className="text-5xl font-bold">Welcome to <span className='font-bold bg-linear-to-l from-[#9514FA] to-[#4F39F6] bg-clip-text text-transparent'>BookNest</span></h1>
+                        <div className="animate__animated animate__rotateIn">
+                            <h1 className="text-5xl font-bold">Welcome to <span className='font-bold bg-linear-to-l from-[#9514FA] to-[#4F39F6] bg-clip-text text-transparent'>BookNest</span></h1>
+                        </div>
                         <p className="py-6">
                             Explore top reads,reviews and stories <br /> that inspire and ignite your imagination
                         </p>
@@ -22,7 +24,7 @@ const Banner = () => {
                 </div>
             </div>
         </div>
-    );
+    ); 
 };
 
 export default Banner;
